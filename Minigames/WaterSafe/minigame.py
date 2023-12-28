2c013c900b573775b05cece820c0985706b8327d
242425 <242425@edu.p.lodz.pl>
2023-12-28 19:36:25 +0100

    minigame poprawki

--- Minigames/WaterSafe/minigame.py
+++ Minigames/WaterSafe/minigame.py
@@ -8,7 +8,7 @@ pygame.init()
 class Player(pygame.sprite.Sprite):
     def __init__(self, width, height):
         super().__init__()
-        self.image = pygame.image.load("/Minigames/WaterSafe/pics/player_icon.png")
+        self.image = pygame.image.load("Minigames/WaterSafe/pics_game/player_icon.png")
         self.image = pygame.transform.scale(self.image, (80, 80))
         self.rect = self.image.get_rect()
         self.rect.center = (width // 2, height // 2)
@@ -51,8 +51,8 @@ class Tap(pygame.sprite.Sprite):
 
     def __init__(self, x, y, water_flow_rate=0.1):
         super().__init__()
-        self.image_zakrecony = pygame.image.load("/Minigames/WaterSafe/pics/tap_icon.png")
-        self.image_odkrecany = pygame.image.load("/Minigames/WaterSafe/pics/odkrecony_tap.png")
+        self.image_zakrecony = pygame.image.load("Minigames/WaterSafe/pics_game/tap_icon.png")
+        self.image_odkrecany = pygame.image.load("Minigames/WaterSafe/pics_game/odkrecony_tap.png")
         self.image = self.image_zakrecony
         self.image = pygame.transform.scale(self.image, (self.TAP_SIZE, self.TAP_SIZE))
         self.rect = self.image.get_rect()
@@ -75,7 +75,7 @@ class NPC(pygame.sprite.Sprite):
 
     def __init__(self, all_taps, game):
         super().__init__()
-        self.image = pygame.image.load("/Minigames/WaterSafe/pics/npc_icon.png")
+        self.image = pygame.image.load("Minigames/WaterSafe/pics_game/npc_icon.png")
         self.image = pygame.transform.scale(self.image, (self.NPC_SIZE, self.NPC_SIZE))
         self.rect = self.image.get_rect()
         self.rect.center = (game.WIDTH // 4, game.HEIGHT // 4)
@@ -111,15 +111,15 @@ class WaterSafeGame:
     def __init__(self, width=800, height=600, duration=30):
         self.WIDTH = width
         self.HEIGHT = height
-        self.PLAYER_SIZE = 80
-        self.TAP_SIZE = 80
+        self.PLAYER_SIZE = int(80 * min(self.WIDTH, self.HEIGHT) / 800)
+        self.TAP_SIZE = int(80 * min(self.WIDTH, self.HEIGHT) / 800)
         self.FPS = 60
         self.WATER_COUNTER = 15
-        self.NPC_SIZE = 50
+        self.NPC_SIZE = int(50 * min(self.WIDTH, self.HEIGHT) / 800)
         self.WHITE = (255, 255, 255)
         self.duration = duration
         self.game_running = False
-        self.start_button_rect = pygame.Rect(self.WIDTH // 3, self.HEIGHT // 2, 200, 50)
+        self.start_button_rect = pygame.Rect(self.WIDTH // 3, self.HEIGHT // 2, int(200 * min(self.WIDTH, self.HEIGHT) / 800), int(50 * min(self.WIDTH, self.HEIGHT) / 800))
         self.start_button_color = (0, 255, 0)
 
         pygame.init()
@@ -127,8 +127,9 @@ class WaterSafeGame:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.DOUBLEBUF | pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
 
-        self.background = pygame.image.load("https://github.com/freszek/io/blob/9d6900fb110fdc8892449c9a8a7e87312a9284eb/Minigames/WaterSafe/pics/background.jpg")
+        self.background = pygame.image.load("Minigames/WaterSafe/pics_game/background.png")
         self.background = pygame.transform.scale(self.background, (self.WIDTH, self.HEIGHT))
+
 
         self.instruction_text = [
             "Witaj w minigrze WaterSafe!",
@@ -225,8 +226,6 @@ class WaterSafeGame:
         npc2 = NPC(all_taps, self)
         all_sprites.add(npc1, npc2)
 
-
-
         elapsed_time = 0
         while self.game_running:
             for event in pygame.event.get():
@@ -247,7 +246,8 @@ class WaterSafeGame:
                     self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
                     for tap in all_taps.sprites():
                         tap.rect.topleft = (random.randint(0, self.WIDTH - self.TAP_SIZE), random.randint(0, self.HEIGHT - self.TAP_SIZE))
-                    player.rect.center = (self.WIDTH // 2, self.HEIGHT // 2)
+                    self.background = pygame.transform.scale(self.background, (self.WIDTH, self.HEIGHT))
+
 
             for tap in all_taps.sprites():
                 if not tap.zakrecony:
