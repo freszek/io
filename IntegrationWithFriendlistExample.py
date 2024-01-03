75672ec1d3e5b8595ad37fcbeec5821c9b175c74
LudzCPP <kacper.janowicz@onet.pl>
2024-01-03 17:31:00 +0100

    Small changes

--- IntegrationWithFriendlistExample.py
+++ IntegrationWithFriendlistExample.py
@@ -1,5 +1,8 @@
 import pygame
 from FriendList import FriendList, User, Button
+from UserDao import UserDao
+
+userdao = UserDao()
 
 pygame.init()
 
@@ -7,14 +10,17 @@ DISPLAY_WIDTH, DISPLAY_HEIGHT = 800, 600
 screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
 example_users = [User(i, f"User{i}") for i in range(12)]
-#users_list = UserDao.get_all()
+# users = userdao.get_all()
+users = userdao.get_friend_list('')
 
 FRIENDS_BUTTON_X = 250
 FRIENDS_BUTTON_Y = 50
 
 toggle_button = Button(FRIENDS_BUTTON_X, FRIENDS_BUTTON_Y, 140, 50, "Znajomi")
 
-friend_list = FriendList(FRIENDS_BUTTON_X - 10, FRIENDS_BUTTON_Y + 50, 140, 500, example_users)
+friend_list = FriendList(FRIENDS_BUTTON_X - 10, FRIENDS_BUTTON_Y + 50, 140, 500, users)
+
+print(users[0].login)
 
 running = True
 while running:
