70eaf9e51e7c45824fbf15cd0d6c4cd6f851c3a2
Hubert Kłosowski <126911966+HubertKlosowski@users.noreply.github.com>
2024-01-04 14:38:28 +0100

    Delete IEventy.py

--- IEventy.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, ABCMeta, abstractmethod
-from QuizWindow import QuizWindow
-
-
-class IEventy(ABC, metaclass=ABCMeta):
-    @abstractmethod
-    def get_event_form(self):
-        QuizWindow()
